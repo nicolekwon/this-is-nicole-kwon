8802cdb36d90b3d0a040968f8024ae4f07a9cf71
nicolekwon-ps <86036139+nicolekwon-ps@users.noreply.github.com>
2023-06-19 19:08:15 -0400

    Added basic layout and start to nav bar

--- nicole-kwon-website/src/App.js
+++ nicole-kwon-website/src/App.js
@@ -1,14 +1,46 @@
 import './App.css';
 import React from 'react';
 import Fade from 'react-reveal/Fade';
+import menu from './icons/menu.png';
+import star from './icons/star.png';
+import photo from './icons/photo.jpg';
+import software_engineer from './icons/software_engineer.JPEG';
 
 function App() {
+
+  React.useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => (document.body.style.overflow = "scroll");
+  });
+
+  const [change, setChange] = React.useState(false);
+
   return (
-    <div className="App">
-      <Fade top duration={2000}>
-        <h1 className="Intro"> hi, i'm nicole </h1>
-      </Fade>
+    <>
+    <div className="container">
+      <div className="navbar">
+        <Fade right when={change} duration={2000}>
+          <div className="navheaders">
+            <p className="headers"> work </p>
+            <p className="headers"> about </p>
+          </div>
+        </Fade>
+        <button className="menu" onClick={() => setChange(!change)}> 
+          <img src={menu} className="menuicon" alt="menuicon"/>
+        </button>
+      </div>
+      <div className="body">
+        <div>
+          <p className="intro"> hi, i'm nicole, </p>
+          <p className="intro"> a software engineer and aspiring product manager </p>
+          <p className="intro"> based in jersey city, new jersey </p>
+        </div>
+        <div>
+          <img src={software_engineer} className="photo" alt="photo"/>
+        </div>
+      </div>
     </div>
+    </>
   );
 }
 
